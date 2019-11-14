6ce55b57f82e78bde637232caa89ab321c48068f
Aditya Jangra <200434215@student.georgianc.on.ca>
2019-11-14 06:21:40 -0500

    Change

--- WebApplicationMusicStore/Controllers/HomeController.cs
+++ WebApplicationMusicStore/Controllers/HomeController.cs
@@ -34,6 +34,12 @@ namespace WebApplicationMusicStore.Controllers
             return View();
         }
 
+        
+        public IActionResult Store()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
